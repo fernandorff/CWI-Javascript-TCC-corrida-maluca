47e93d19c72316063b68e1c224944831e9356797
Fernando Rocha <fernandorff92@gmail.com>
2022-10-22 13:43:12 -0300

    atualizando

--- corrida.js
+++ corrida.js
@@ -4,12 +4,17 @@ export async function criaPersonagem(lista, personagemID) {
   const getPersonagem = await axios.get(lista).then((response) => response);
   const personagem = getPersonagem.data[personagemID];
   personagem.posicao = 0;
+  personagem.buff = 0;
   return personagem;
 }
 
 export async function criaPista(lista, pistaID) {
   const getPista = await axios.get(lista).then((response) => response);
   const pista = getPista.data[pistaID];
+  pista.buffs = [];
+  pista.posicoesBuffs.forEach((element) => {
+    pista.buffs.push({ posicao: element, valor: 0, buffados: [] });
+  });
   return pista;
 }
 
@@ -24,50 +29,74 @@ export async function criaCorrida(pista, ...personagens) {
   let corrida = pista;
   corrida.participantes = [];
 
-  personagens.forEach((element) => {
-    corrida.participantes.push(element);
+  personagens.forEach((elementPersonagem) => {
+    corrida.participantes.push(elementPersonagem);
   });
   return corrida;
 }
 
 export async function executarCorrida(corrida) {
-  for (let rodada = 1; rodada < 10; rodada++) {
-    if (condition) {
-    }
+  for (let rodada = 1; rodada < 12; rodada++) {
+    corrida.buffs.forEach((elementBuff) => {
+      let buffouAlguem = false;
+      corrida.participantes.forEach((elementPersonagem) => {
+        if (elementBuff.buffados.includes(elementPersonagem.nome) == false) {
+          if (elementPersonagem.posicao >= elementBuff.posicao) {
+            elementBuff.buffados.push(elementPersonagem.nome);
+            elementPersonagem.buff += elementBuff.valor;
+            buffouAlguem = true;
+          }
+        }
+      });
+      if (buffouAlguem) {
+        elementBuff.valor += 1;
+      }
+    });
 
-    corrida.participantes.forEach((element) => {
+    console.log("buffados: ", corrida.buffs);
+
+    corrida.participantes.forEach((elementPersonagem) => {
       switch (true) {
         case rodada <= 3:
-          element.posicao += element.aceleracao;
-          console.log(
-            "RODADA ",
-            rodada,
-            "RANDANDANDAN",
-            element.nome,
-            element.posicao
-          );
+          elementPersonagem.posicao +=
+            elementPersonagem.aceleracao + elementPersonagem.buff;
+          console.table([
+            [
+              elementPersonagem.nome,
+              `Rodada: ${rodada}`,
+              "Acelerando",
+              `Posicao: ${elementPersonagem.posicao}`,
+              `Buff: ${elementPersonagem.buff}`,
+            ],
+          ]);
           break;
 
         case rodada % 4 == 0:
-          element.posicao += element.drift;
-          console.log(
-            "RODADA ",
-            rodada,
-            "DORIFITOOO",
-            element.nome,
-            element.posicao
-          );
+          elementPersonagem.posicao +=
+            elementPersonagem.drift + elementPersonagem.buff;
+          console.table([
+            [
+              elementPersonagem.nome,
+              `Rodada: ${rodada}`,
+              "Drift",
+              `Posicao: ${elementPersonagem.posicao}`,
+              `Buff: ${elementPersonagem.buff}`,
+            ],
+          ]);
           break;
 
         default:
-          element.posicao += element.velocidade;
-          console.log(
-            "RODADA ",
-            rodada,
-            "VRUMMMMM",
-            element.nome,
-            element.posicao
-          );
+          elementPersonagem.posicao +=
+            elementPersonagem.velocidade + elementPersonagem.buff;
+          console.table([
+            [
+              elementPersonagem.nome,
+              `Rodada: ${rodada}`,
+              "Velocidade",
+              `Posicao: ${elementPersonagem.posicao}`,
+              `Buff: ${elementPersonagem.buff}`,
+            ],
+          ]);
           break;
       }
     });
